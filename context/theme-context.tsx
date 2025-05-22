99eb47241646f20179b20d39e248f8e6e72e0c1a
ARYAN GUPTA <911aaryan@gmail.com>
2025-05-22 23:18:20 +0530

    Update theme-context.tsx

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -9,6 +9,7 @@ interface ThemeContextType {
   theme: Theme
   toggleTheme: () => void
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
